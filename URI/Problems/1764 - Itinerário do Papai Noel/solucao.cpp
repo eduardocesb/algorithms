80d48b5c981ba26cf7c7f55802262efa72518a29
Carlos Eduardo <carloseduardo_s_bastos@hotmail.com>
2016-11-29 20:22:07 -0300

    Adicionado a solução para a  questão '1764 - Itinerário do Papai Noel'

--- URI/Problems/1764 - Itinerário do Papai Noel/solucao.cpp
+++ URI/Problems/1764 - Itinerário do Papai Noel/solucao.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cstring>
 
 using namespace std;
 
@@ -47,10 +48,66 @@ public:
 		Aresta aresta(v1, v2, peso);
 		arestas.push_back(aresta);
 	}
+
+	int buscar(int subset[], int i){
+		if(subset[i] == -1){
+			return i;
+		}
+		return buscar(subset, subset[i]);
+	}
+
+	void unir(int subset[], int v1, int v2){
+		int v1_set = buscar(subset, v1);
+		int v2_set = buscar(subset, v2);
+		subset[v1_set] = v2_set;
+	}
+
+	int kruskal(){
+		vector<Aresta> arvore;
+		int size_arestas = arestas.size();
+
+		sort(arestas.begin(), arestas.end());
+
+		int * subset = new int[V];
+
+		memset(subset, -1, sizeof(int) * V);
+		for (int i = 0; i < size_arestas; ++i)
+		{
+			int v1 = buscar(subset, arestas[i].obterVertice1());
+			int v2 = buscar(subset, arestas[i].obterVertice2());
+
+			if(v1 != v2){
+				arvore.push_back(arestas[i]);
+				unir(subset, v1, v2);
+			}
+		}
+		int size_arvore = arvore.size();
+
+		int r = 0;
+		for (int i = 0; i < size_arvore; ++i)
+		{
+			r += arvore[i].obterPeso();
+		}
+		return r;
+	}
+
 };
 
 int main(int argc, char const *argv[])
 {
-	
+	int M, N;
+	cin >> M >> N;
+	while(M != 0 || N != 0){
+		Grafo g(M);
+		for (int i = 0; i < N; ++i)
+		{
+			int v1, v2, custo;
+			cin >> v1 >> v2 >> custo;
+			g.adicionarAresta(v1, v2, custo);
+		}
+
+		cout << g.kruskal() << endl;
+		cin >> M >> N;
+	}
 	return 0;
 }
