13e66087abf2228774c7fa97601ace2eaafc5ea1
Carlos Eduardo <eduardo@localhost.localdomain>
2017-07-25 18:36:39 -0300

    Adicionado a solução da questão 'Véi, dá meu troco' do assunto 'Troco'

--- CODCAD/Problemas/Troco/Véi, dá meu troco/solucao.cpp
+++ CODCAD/Problemas/Troco/Véi, dá meu troco/solucao.cpp
@@ -7,17 +7,18 @@ int dp[MAXN], N, M;
 vector<int> moedas;
 
 bool troco(int x){
-	if(x < 0) return false;
-	if(x == 0) return true;
-
+	if(x < 0) return 0;
+	if(x == 0) return 1;
+	if(dp[x] > -1) return dp[x];
 	for (int i = 0; i < N; ++i)
-		if(troco(x - moedas[i])) return dp[x - moedas[i]] = true;
-	return dp[x] = false;
+		if(troco(x - moedas[i])) return dp[x - moedas[i]] = 1;
+	return dp[x] = 0;
 }
 
 
 int main(int argc, char const *argv[])
 {
+	memset(dp, -1, sizeof(dp));
 	cin >> N >> M;
 	for (int x, i = 0; i < N; ++i)
 	{
