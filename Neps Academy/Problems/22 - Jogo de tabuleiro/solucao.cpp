1b33f16926771295e64fd93019af46f61e8920ff
Carlos Eduardo <carloseduardo@ifpi.edu.br>
2020-10-08 00:10:18 -0300

    Renamed

--- Neps Academy/Problems/22 - Jogo de tabuleiro/solucao.cpp
+++ Neps Academy/Problems/22 - Jogo de tabuleiro/solucao.cpp
@@ -1,47 +1,54 @@
 #include <bits/stdc++.h>
+#define optimize ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define INF 2000000020LL
 #define ALL(x) x.begin(), x.end()
+#define UNIQUE(c) (c).resize(unique(ALL(c)) - (c).begin())
+#define REP(i, a, b) for(int i = (a); i <= (b); i++)
+#define POS(n, x) (lower_bound(ALL(n), x) - (n).begin())
 #define ll long long
 #define ld long double
+#define pii pair<int,int>
+#define vi vector<int>
+#define vii vector<pii>
 #define PB push_back
 #define EB emplace_back
-#define MAXN 2000020
-
+#define MOD 1000000007
+#define PRIME 101 
+#define MAXN 1010
+#define MAXL 23
+#define endl '\n'
 
 using namespace std;
 
-int main(int argc, char const *argv[]){
-    int n;
-    cin >> n;
-    int tab [n+1][n+1];
-    int b = 0, p = 0;
+// (1 + sqrt(5)) / 2
+const ld GOLDEN_RATIO = 1.6180339887498949025257388711906969547271728515625;
+const ld PI = 2.0 * acos(0.0);
+const int BLOCK = 333;
 
-    for (int i = 1; i <= n; i++){
-        for (int j = 1; j <= n; j++){
-            cin >> tab[i][j];
-        }
-    }
+int dp[MAXN][MAXN];
 
-    for (int i = 2; i <= n; i++){
-        for (int j = 2; j <= n; j++){
+int main(int argc, char const *argv[])
+{
+	optimize;
+	
+	int N;
 
-            p = 0;
-            b = 0;
+	cin >> N;
 
-            if (tab[i-1][j-1] == 1) p++;
-            else b++;
-            if (tab[i][j-1] == 1) p++;
-            else b++;
-            if (tab[i-1][j] == 1) p++;
-            else b++;
+	for(int i = 1; i <= N; i++)
+		for(int j = 1; j <= N; j++)
+			cin >> dp[i][j];
 
-            if (b < p) tab[i][j] = 0;
-            else tab[i][j] = 1;
-            
-        }
-    }
+	for(int i = 2; i <= N; i++)
+		for(int j = 2; j <= N; j++)
+		{
+			int p = (dp[i - 1][j - 1] == 1) + (dp[i - 1][j] == 1) + (dp[i][j - 1] == 1);
+			int b = 3 - p;
 
-    cout << tab[n][n] << endl;
+			dp[i][j] = p < b;
+		}
 
-    return 0;
-}
+	cout << dp[N][N] << endl;
+
+	return 0;
+};
--- Neps Academy/Problems/24 - Castelos da Nlogônia/solucao.cpp
+++ Neps Academy/Problems/24 - Castelos da Nlogônia/solucao.cpp
@@ -1,67 +1,155 @@
 #include <bits/stdc++.h>
+#define optimize ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define INF 2000000020LL
 #define ALL(x) x.begin(), x.end()
+#define UNIQUE(c) (c).resize(unique(ALL(c)) - (c).begin())
+#define REP(i, a, b) for(int i = (a); i <= (b); i++)
+#define POS(n, x) (lower_bound(ALL(n), x) - (n).begin())
 #define ll long long
 #define ld long double
+#define pii pair<int,int>
+#define vi vector<int>
+#define vii vector<pii>
 #define PB push_back
 #define EB emplace_back
-#define MAXN 2000020
+#define MOD 1000000007
+#define PRIME 101 
+#define MAXN 200020
+#define MAXL 23
+#define endl '\n'
 
 using namespace std;
 
-int n, m;
-vector <int> grafo [120];
-int pai[120], cor [120];
+// (1 + sqrt(5)) / 2
+const ld GOLDEN_RATIO = 1.6180339887498949025257388711906969547271728515625;
+const ld PI = 2.0 * acos(0.0);
+const int BLOCK = 333;
 
-void dfs (int u, int p){
+vi grafo[MAXN];
+int pai[MAXN], nivel[MAXN], cor[MAXN];
 
-    for (auto vizinho : grafo[u]){
-        
-        if (vizinho != p){
-            pai[vizinho] = u;
+namespace seg {
+	ll seg[4*MAXN], lazy[4*MAXN];
+	int n, *v;
 
-            dfs(vizinho, u);
-        }
+	ll build(int p=1, int l=0, int r=n-1) {
+		lazy[p] = -1;
+		if (l == r) return seg[p] = 0;
+		int m = (l+r)/2;
+		return seg[p] = build(2*p, l, m) + build(2*p+1, m+1, r);
+	}
+	void build(int n2, int* v2) {
+		n = n2, v = v2;
+		build();
+	}
+	void prop(int p, int l, int r) {
+		if (lazy[p] == -1) return;
+		int m = (l+r)/2;
+		seg[2*p] = lazy[p]*(m-l+1);
+		seg[2*p+1] = lazy[p]*(r-(m+1)+1);
+		lazy[2*p] = lazy[p], lazy[2*p+1] = lazy[p];
+		lazy[p] = -1;
+	}
 
-    }
+	ll query(int a, int b, int p=1, int l=0, int r=n-1) {
+		if (b < l or r < a) return 0;
+		if (a <= l and r <= b) return seg[p];
+		prop(p, l, r);
+		int m = (l+r)/2;
+		return query(a, b, 2*p, l, m) + query(a, b, 2*p+1, m+1, r);
+	}
+
+	ll update(int a, int b, int x, int p=1, int l=0, int r=n-1) {
+		if (b < l or r < a) return seg[p];
+		if (a <= l and r <= b) {
+			seg[p] = (ll)x*(r-l+1);
+			lazy[p] = x;
+			return seg[p];
+		}
+		prop(p, l, r);
+		int m = (l+r)/2;
+		return seg[p] = update(a, b, x, 2*p, l, m) +
+			update(a, b, x, 2*p+1, m+1, r);
+	}
+};
+
+namespace hld {
+	vector<int> g[MAXN];
+	int pos[MAXN], sz[MAXN];
+	int peso[MAXN], pai[MAXN];
+	int h[MAXN], v[MAXN], t;
+
+	void build_hld(int k, int p = -1, int f = 1) {
+		v[pos[k] = t++] = peso[k]; sz[k] = 1;
+		for (auto& i : g[k]) if (i != p) {
+			pai[i] = k;
+			h[i] = (i == g[k][0] ? h[k] : i);
+			build_hld(i, k, f); sz[k] += sz[i];
+
+			if (sz[i] > sz[g[k][0]] or g[k][0] == p) swap(i, g[k][0]);
+		}
+		if (p*f == -1) build_hld(h[k] = k, -1, t = 0);
+	}
+
+	void build(int root = 0) {
+		t = 0;
+		build_hld(root);
+		seg::build(t, v);
+	}
+
+	ll query_path(int a, int b) {
+		if (pos[a] < pos[b]) swap(a, b);
+
+		if (h[a] == h[b]) return seg::query(pos[b], pos[a]);
+		return seg::query(pos[h[a]], pos[a]) + query_path(pai[h[a]], b);
+	}
+	void update_path(int a, int b, int x) {
+		if (pos[a] < pos[b]) swap(a, b);
+
+		if (h[a] == h[b]) return (void)seg::update(pos[b], pos[a], x);
+		seg::update(pos[h[a]], pos[a], x); update_path(pai[h[a]], b, x);
+	}
+	ll query_subtree(int a) {
+		return seg::query(pos[a], pos[a]+sz[a]-1);
+	}
+	void update_subtree(int a, int x) {
+		seg::update(pos[a], pos[a]+sz[a]-1, x);
+	}
+	int lca(int a, int b) {
+		if (pos[a] < pos[b]) swap(a, b);
+		return h[a] == h[b] ? b : lca(pai[h[a]], b);
+	}
 }
 
-int main(int argc, char const *argv[]){
+int main(int argc, char const *argv[])
+{
+	optimize;
+	
+	int N, M, u, v, c;
 
-    for (int i = 0; i < 120; i++){
-        cor[i] = 0;
-    }
+	cin >> N >> M;
 
-    cin >> n >> m;
+	for(int i = 0; i < MAXN; i++) hld::peso[i] = hld::v[i] = 0;
 
-    for (int i = 1; i < n; i++){
-        int u, v;
-        cin >> u >> v;
-        grafo[u].push_back(v);
-        grafo[v].push_back(u);
-    }
+	for(int i = 1; i < N; i++)
+	{
+		cin >> u >> v;
 
-    for (int i = 0; i < m; i++){
+		hld::g[u - 1].PB(v - 1);
+		hld::g[v - 1].PB(u - 1);
+	}
 
-        for (int j = 0; j < 120; j++){
-            pai[j] = -1;
-        }
-        int p, q, c;
-        cin >> p >> q >> c;
-        dfs (p, -1);
-        cor[p] = c;
-        while (q != p) {
-            cor[q] = c;
-            q = pai[q];
-            cor[q] = c;
-        }
-    }
+	hld::build();
 
-    for (int i = 1; i <= n; i++){
-        cout << cor[i] << " ";
-    }
-    
-    cout << endl;
-    
-    return 0;
+	while(M--)
+	{
+		cin >> u >> v >> c;
+
+		hld::update_path(u - 1, v - 1, c);
+	}
+
+	for(int i = 1; i <= N; i++)
+		cout << hld::query_path(i - 1, i - 1) << " \n"[i == N];
+
+	return 0;
 }
