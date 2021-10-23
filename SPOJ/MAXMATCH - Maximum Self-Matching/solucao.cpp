c56ecf2e5e3743a5d2ef78cc9100b360d9c84e4f
Leonardo Su <leonardosu0@gmail.com>
2021-10-23 12:32:56 -0300

    Choreadd maxmatch (#21)
    * Solved A
    
    * Solved B
    
    * Solved C
    
    * Solved D
    
    * Solved E
    
    * solved MAXMATCH

---
+++ SPOJ/MAXMATCH - Maximum Self-Matching/solucao.cpp
@@ -0,0 +1,93 @@
+#include "bits/stdc++.h" //https://www.spoj.com/problems/MAXMATCH/
+using namespace std;	// Leonardo
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+typedef complex<double> C;
+typedef vector<double> vd;
+
+void fft(vector<C>& a) 
+{
+	int n = sz(a), L = 31 - __builtin_clz(n);
+	static vector<complex<long double>> R(2, 1);
+	static vector<C> rt(2, 1);
+	for (static int k = 2; k < n; k *= 2) 
+	{
+		R.resize(n); rt.resize(n);
+		auto x = polar(1.0L, acos(-1.0L) / k);
+		rep(i,k,2*k) rt[i] = R[i] = i&1 ? R[i/2] * x : R[i/2];
+	}
+	vi rev(n);
+	rep(i,0,n) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
+	rep(i,0,n) if (i < rev[i]) swap(a[i], a[rev[i]]);
+	for (int k = 1; k < n; k *= 2)
+		for (int i = 0; i < n; i += 2 * k) rep(j,0,k) 
+		{
+			C z = rt[j+k] * a[i+j+k];
+			a[i + j + k] = a[i + j] - z;
+			a[i + j] += z;
+		}
+}
+
+vd conv(const vd& a, const vd& b) 
+{
+	if (a.empty() || b.empty()) return {};
+	vd res(sz(a) + sz(b) - 1);
+	int L = 32 - __builtin_clz(sz(res)), n = 1 << L;
+	vector<C> in(n), out(n);	copy(all(a), begin(in));
+	rep(i,0,sz(b)) in[i].imag(b[i]);
+	fft(in);
+	for (C& x : in) x *= x;
+	rep(i,0,n) out[i] = in[-i & (n - 1)] - conj(in[i]);
+	fft(out);
+	rep(i,0,sz(res)) res[i] = imag(out[i]) / (4 * n);
+	return res;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	string s,rs;
+	cin>>s;
+	rs = s;
+
+	reverse(ALL(rs));
+
+	int n = sz(s);
+	vd a(n),b(n);
+
+	array<vd, 3> letters;
+
+	for(char letter='a';letter<='c';++letter)
+	{
+		for(int i=0;i<n;++i)
+		{
+			a[i] = (s[i] == letter) ? 1.0 : 0.0;
+			b[i] = (rs[i] == letter) ? 1.0 : 0.0;
+		}
+
+		letters[letter-'a'] = conv(a,b);
+	}
+
+
+	int best = 0;
+	vi ans(n);
+	for(int i = 1; i < n; ++i) 
+	{
+		for(int letter=0;letter<3;++letter)
+			ans[i] += (int)(letters[letter][n-1+i] + 0.5);        
+        best = max(best, ans[i]);
+    }
+
+	cout<<best<<"\n";
+	for(int i=1;i<n;++i)
+		if(ans[i] == best)
+			cout<<i<<" ";
+	
+	cout<<"\n";
+}
